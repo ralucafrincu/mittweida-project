cf1c3c5c885e086ef0b8da34a66f73d6b802895d
Frincu Raluca <frincu.raluca0501@yahoo.com>
2025-07-12 05:03:56 +0200

    fixing markers

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,17 @@
 import {MapContainer, Marker, TileLayer} from "react-leaflet";
 import styles from "./Map.module.css";
 import RoutingControl from "./RoutingControl.tsx";
-
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export default function Map({path, markerPath} : {path?: [number, number][], markerPath?: [number, number][]}) {
+
+    //fixing markers not getting rendered on phone
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+        iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+        shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+    })
 
     const position :[number, number] = [50.9856, 12.9810];
 
